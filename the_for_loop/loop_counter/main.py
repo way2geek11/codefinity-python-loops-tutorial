408ac3c851d5549139cc4f29e3277a27c27c0686
way2geek11 <>
2026-06-09 03:03:44 +0000

    Loop counter

---
+++ the_for_loop/loop_counter/main.py
@@ -0,0 +1,14 @@
+# List of travel destinations
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# Counter variable
+counter = 0
+
+for country in countries:
+    if len(country)==6:
+        counter+=1
+
+
+
+# Testing
+print("Number of countries with exactly 6 letters:", counter)
