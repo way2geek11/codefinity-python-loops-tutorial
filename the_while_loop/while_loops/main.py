bb8b4e7055b829e49db7c94e67ee8c3fe3e2a368
way2geek11 <>
2026-06-13 17:07:31 +0000

    While loops

---
+++ the_while_loop/while_loops/main.py
@@ -0,0 +1,15 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+i=0
+    
+# List to hold the selected countries
+selected = []
+
+while i < len(countries)//2:
+    #for c in countries:
+    selected.append(countries[i])
+    i+=1
+
+
+# Testing
+print('Selected countries:', selected)
